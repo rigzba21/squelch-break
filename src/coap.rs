5ab770dba175434c4983434fafee4aec16cf7383
rigzba21 <rigbynined@gmail.com>
2021-12-23 21:49:39 -0500

    adding some tests...
    Signed-off-by: rigzba21 <rigbynined@gmail.com>

--- src/coap.rs
+++ src/coap.rs
@@ -5,27 +5,32 @@ use bytes::{Bytes};
 
 // CoAP RFC 7252 3. Message Format
 // https://datatracker.ietf.org/doc/html/rfc7252#section-3
+
+#[derive(Debug)]
 pub struct Message {
     pub version: u8,
     pub msg_type: u8,
-    pub token_length: u8,
-    pub code: Bytes,
-    pub message_id: Bytes,
+    pub token_length: usize,
+    pub code: u8,
+    pub message_id: u16,
     pub token: Token,
     pub options: Options,
     pub payload: Bytes,
 }
 
+#[derive(Debug)]
 pub struct Token {
     pub bytes: Bytes,
 }
 
+#[derive(Debug)]
 pub struct Options {
-    pub option_delta: Bytes,
-    pub option_length: Bytes,
-    pub option_val: OptionValue,
+    pub option_delta: u8,
+    pub option_length: u8,
+    pub option_val: u8,
 }
 
+#[derive(Debug)]
 pub enum OptionValue {
     Empty,
     Opaque,
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,6 @@
 use bytes::{Bytes};
 
-use squelch_break::{Token, Message};
+use squelch_break::{Token, Message, Options};
 
 #[test]
 fn it_works() {
@@ -24,13 +24,26 @@ fn test_sample_message() {
     };
     
     let token_length = token.bytes.len();
+
+    let payload = Bytes::from("Some dummy payload");
+
+    let options = Options {
+        option_delta: 0x010,
+        option_length: 0xab,
+        option_val: 0x00, //empty  options
+    };
     
-
-
-    /*let message = Message {
+    let message = Message {
         version: 0x01,
         msg_type: 0x00,
-        token_length: 
-    }*/
+        token_length: token_length,
+        code: 0x00000011,
+        message_id: 0x0000000000000001,
+        token: token,
+        options: options,
+        payload: payload,
+    };
+
+    println!("{:#?}", message);
 }
 
